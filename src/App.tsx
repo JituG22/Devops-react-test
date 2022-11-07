a927f6178e2389d214170728afe248cc631aa652
JituG22 <jitu123435@gmail.com>
2022-11-07 09:36:22 +0530

    test 2

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-         this is an sample 
+        This is an test 2 
       </header>
     </div>
   );
